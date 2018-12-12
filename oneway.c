bbf78423a94211635afb7179ebf7166fad7c3513
Suva Shahria <ss2479@top.cs.rutgers.edu>
2018-12-12 07:16:16 -0500

    hi michael

--- oneway.c
+++ oneway.c
@@ -14,6 +14,46 @@ void extend_one_hand(int threadId, int threadNum, GraphData graph,
                      int *res, int *strongNeighbor)
 {
     /* Your Code Goes Here */
+	//printf("%d \n",strongNeighbor[2]);
+	int i = threadId;
+	int wc = (nodeNum + threadNum - 1) / threadNum;
+	int beg = i * wc;
+	int end;
+	if( (beg+wc) < nodeNum){
+		end = beg+wc;
+	}else{
+		end = nodeNum;
+	}
+	// end = min(beg + wc, nodeNum);
+
+	//end = beg +5; testing done
+	for(i=beg; i<end; i++){  //go through assigned vertices from nodetoprocess
+		int idx = nodeToProcess[i];
+		int degree = graph.degree[idx];
+		int offset = graph.offset[idx];
+		int poff = offset;
+		int idx2 =0;
+		int str;
+
+		for(idx2 = 0; idx2 < degree;idx2++){
+			str = graph.index[offset];
+			offset++;
+			if(res[str] == UNMATCHED){
+
+				strongNeighbor[i] = str;
+				idx2 =degree+1; // break loop
+			}
+
+		}
+
+		//printf("%d %d %d %d %d %d %lf\n",idx, degree,poff,str,strongNeighbor[i],graph.index[0], graph.weight[0]);
+
+	}
+
+
+
+	//printf("%d \n",strongNeighbor[0]);
+
 }
 
 void check_handshaking(int threadId, int threadNum,
@@ -21,6 +61,30 @@ void check_handshaking(int threadId, int threadNum,
                        int *strongNeighbor, int *res)
 {
     /* Your Code Goes Here */
+
+	int i = threadId;
+	int wc = (nodeNum + threadNum - 1) / threadNum;
+	int beg = i * wc;
+	int end;
+	if( (beg+wc) < nodeNum){
+		end = beg+wc;
+	}else{
+		end = nodeNum;
+	}
+
+	int idx;
+	int str;
+	for(i=beg; i<end; i++){
+		idx = nodeToProcess[i];
+		str = strongNeighbor[idx];
+		if(strongNeighbor[str] == idx){ // match
+		//	printf("%d %d %d %d\n",str,idx, strongNeighbor[str],strongNeighbor[idx]);
+			res[idx] = str;
+		}
+	}
+
+
+
 }
 
 
